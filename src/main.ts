f0afd0fb709d58a6130f242fd7bbaa85181efa1a
Daniel Holzmann <d@velopment.at>
2022-11-28 10:54:06 +0100

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,27 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 async function run() {
-    const projectKeys = core.getInput('project-keys').split(',')
-        .map(key => key.trim().toUpperCase())
-        .filter(key => !!key);
+  const projectKeys = core
+    .getInput("project-keys")
+    .split(",")
+    .map((key) => key.trim().toUpperCase())
+    .filter((key) => !!key);
 
-    if (projectKeys.length === 0) {
-        core.setFailed('please provide a comma separated list of project keys to use')
-    }
+  if (projectKeys.length === 0) {
+    core.setFailed(
+      "please provide a comma separated list of project keys to use"
+    );
+  }
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`the event payload: ${payload}`);
+  const payload = JSON.stringify(
+    github.context.payload.pull_request,
+    undefined,
+    2
+  );
+  console.log(`the event payload: ${payload}`);
 
-    console.log(`project key: ${JSON.stringify(projectKeys, undefined, 2)}`)
+  console.log(`project key: ${JSON.stringify(projectKeys, undefined, 2)}`);
 }
 
 run();
